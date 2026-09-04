65e7ed4b0009c2e3dba48caca7c8c05e71ca60c3
Rylan Meilutis <rylan.meilutis@gmail.com>
2026-09-04 10:28:22 -0400

    Use warning-free SEDSNet routing build

--- cmake/sedsnet_fetch.cmake
+++ cmake/sedsnet_fetch.cmake
@@ -13,7 +13,7 @@ set(SEDSNET_ENABLE_CRYPTOGRAPHY OFF CACHE BOOL
 FetchContent_Declare(
     sedsnet
     GIT_REPOSITORY https://github.com/Rylan-Meilutis/SEDSnet.git
-    GIT_TAG 3b2eb63edc17aa8feb14b1f90d6bb91cbb417752
+    GIT_TAG 3638f9b832af7cab519bdd5ed75cd85d395cbca8
     GIT_SHALLOW FALSE
     PATCH_COMMAND ${CMAKE_COMMAND}
                   -DSEDSNET_SOURCE_DIR=<SOURCE_DIR>
